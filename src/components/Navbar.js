6bb312f20f2d6db0ad11bc295f79b501fc937f32
Alberto <a.p.r.13287@gmail.com>
2020-11-24 16:43:08 +0100

    prueba

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,27 +6,30 @@ class Navbar extends Component {
   render() {
     const { user, logout, isLoggedin } = this.props;
     return (
-      <nav className='navbar'>
-        <Link to={"/"} id='home-btn'>
+      <nav className="navbar">
+        <Link to={"/"} id="home-btn">
           <h4>Home</h4>
         </Link>
         {isLoggedin ? (
           <>
-            
-            <button className='navbar-button' onClick={logout}>
+            <button className="navbar-button" onClick={logout}>
               Logout
             </button>
-          
+            <div className="narvbar-perfil">
+              <Link to={`/gettingProfile/${this.props.user._id}`}>
+                Ir a profile
+              </Link>
+            </div>
+          </>
         ) : (
           <>
-            <Link to='/login'>
-              <button className='navbar-button'>Login</button>
+            <Link to="/login">
+              <button className="navbar-button">Login</button>
             </Link>
             <br />
-            <Link to='/signup'>
-              <button className='navbar-button'>Sign Up</button>
+            <Link to="/signup">
+              <button className="navbar-button">Sign Up</button>
             </Link>
-            
           </>
         )}
       </nav>
